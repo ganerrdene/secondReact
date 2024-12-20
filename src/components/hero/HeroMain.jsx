e6710be75c465ba39c3ba511c312b561ba62f169
24LP7064 24LP7064 <24LP7064@admins-iMac.local>
2024-12-20 12:36:27 +0800

    shine

--- src/components/hero/HeroMain.jsx
+++ src/components/hero/HeroMain.jsx
@@ -1,3 +1,16 @@
+const medeenuud = [
+  {
+    category: "Уул уурхай",
+    medee: [
+      {
+        title: "Монголын литийн нөөц нэмэгдэж 680,000 тн-д хүрээд байна",
+        publisher: "М.Хулан / 12 сарын 20, 2024",
+        imgSrc:
+          "https://dash-api.yld.mn/api/image/5df9f347-0e55-42d0-afc2-b438ec13a97f",
+      },
+    ],
+  },
+];
 export const HeroMain = () => {
-  return <div>HeroMain</div>;
+  return <div></div>;
 };
